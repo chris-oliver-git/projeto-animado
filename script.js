b3a890245336238dd38c886dff6936910deed08a
Christopher Silva <109239723+chris-oliver-git@users.noreply.github.com>
2025-07-05 14:11:00 -0300

    update CSS

--- script.js
+++ script.js
@@ -15,4 +15,8 @@ next.onclick = () => {
 prev.onclick = () => {
     let activeOld = document.querySelector('.active')
     activeOld.classList.remove('active')
+
+    active = active <= 0 ? count -1 : active - 1
+    list[active].classList.add('active')
 }
+
